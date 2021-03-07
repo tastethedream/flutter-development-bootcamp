cee32192e62f6d376be9b01b09a0870c60f82a1e
Dawn Rose <tastethedream@gmail.com>
2021-03-07 09:57:54 +0000

    housekeeping

--- i_am_happy/android/app/src/main/kotlin/dawnrose/i_am_happy/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dawnrose.i_am_happy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- i_am_rich/android/app/src/main/kotlin/dawnrose/i_am_rich/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dawnrose.i_am_rich
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
